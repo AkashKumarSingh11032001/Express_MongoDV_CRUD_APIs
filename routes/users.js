9e0985b01be60edc85a3232463c63366a937c1ff
Akash <akashkumarsingh11032001@gmail.com>
2023-04-08 19:58:59 +0530

    create new users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { createUser } from "../controllers/user.js";
 
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.send("Getting Users!");
-});
+// router.get('/',(req,res)=>{
+//     res.send("Getting Users!");
+// });
+
+router.post('/', createUser)
 
 export default router;
