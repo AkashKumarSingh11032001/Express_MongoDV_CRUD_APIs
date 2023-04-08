dc4df118036b729eb05c84c1c8196c8519ac382d
Akash <akashkumarsingh11032001@gmail.com>
2023-04-08 19:26:01 +0530

    connection server to mongoDB

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+
+const app = express()
+const PORT = process.env.PORT || 3000 
+
+// connection server to mongoDB
+const connectDB = async() => {
+    try {
+        mongoose.connect(process.env.DB_CONNECTION)
+        console.log("MongoDB Connected")
+    }catch{
+        console.log(err.message)
+        process.exit(1)
+    }
+}
+
+app.listen(PORT, ()=>{
+    connectDB();
+    console.log(`PORT listing at ${PORT}`)
+});
