d9dcd9a733f2f1b6dc812397322d12f87d4b62a9
Akash <akashkumarsingh11032001@gmail.com>
2023-04-09 10:26:45 +0530

    controller routes defined

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,22 @@
 import express from "express";
-import { createUser } from "../controllers/user.js";
+import { createUser, deleteUser, getAllUser, getUser, updateUser } from "../controllers/user.js";
 
 const router = express.Router();
 
 // create a user
 router.post("/", createUser);
+
+// get all user
+router.get("/", getAllUser)
+
+// get user by ID
+router.get("/:userId",getUser)
+
+// update user by ID
+router.patch("/:userId", updateUser);
+
+// deleter user by ID
+router.delete("/:userId", deleteUser);
 
 // router.get('/',(req,res)=>{
 //     res.send("Getting Users!");
